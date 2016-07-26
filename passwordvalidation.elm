105d4499c75f72fadd8f377cbb55b91d6da326cc
E <lighterbeat@outlook.com>
2016-07-26 16:46:43 -0500

    Update passwordvalidation.elm

--- passwordvalidation.elm
+++ passwordvalidation.elm
@@ -1,7 +1,7 @@
 import Html exposing (..)
 import Html.App as Html
 import Html.Attributes exposing(..)
-import Html.Events exposing(onInput, onClick)
+import Html.Events exposing(..)
 import String
 import Char
 main = 
@@ -12,11 +12,12 @@ type alias Model =
   { name : String 
   , password : String 
   , passwordAgain : String
+  , age: String
   }
 
 
 model : Model 
-model = Model "" "" ""
+model = Model "" "" "" ""
   
 
 
@@ -25,7 +26,7 @@ model = Model "" "" ""
 
 --update
 
-type Msg = Name String | Password String | PasswordAgain String
+type Msg = Name String | Password String | PasswordAgain String | Age String
 
 update : Msg->Model->Model
 update msg model = 
@@ -38,6 +39,8 @@ update msg model =
     
     PasswordAgain password ->
       {model | passwordAgain = password}
+    Age age ->
+      {model | age = age}
   
       
       
@@ -48,12 +51,14 @@ view model =
   [input [type' "text", placeholder "Name", onInput Name][]
   ,input [type' "password", placeholder "Password", onInput Password][]
   ,input [type' "password", placeholder "Password Again", onInput PasswordAgain][]
+  ,input [type' "text", placeholder "Age", onInput Age][]
   ,viewValidation model
   ]
   
   
 viewValidation : Model -> Html msg 
 viewValidation model = 
+  
   let 
          
     (color, message) = 
@@ -62,9 +67,9 @@ viewValidation model =
           String.any Char.isLower model.password &&
           String.length model.password > 8 
       then
-        ("black", "")
+        ("green", "")
       else 
-        ("black", "Password should have a number, upper, lower and minimum 8 characters")
+        ("red", "Password should have a number, upper, lower and minimum 8 characters")
         
     match = 
       if model.password == model.passwordAgain 
@@ -72,9 +77,19 @@ viewValidation model =
       then 
         ""
        else 
-         "Password do not match"   
+         "Password do not match"  
+         
+    age = 
+      if String.all Char.isDigit model.age then 
+       ""
+      else
+       "Must be a number"
+       
+         
   in 
    div [] [
-   div [style [("color", color)]] [ (text message)]
-   , (text match) 
+    button [ type' "button" ]  [  text "Submit" ] 
+   , div [style [("color", color)]] [ h3[] [text message] ]
+   , div [] [ h2 []  [ text match ] ]
+   , h2 [] [ text age ]
    ]
